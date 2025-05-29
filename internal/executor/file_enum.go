36bab84167922b63dcf7728bf429cd7ef4a3c77c
zeusnotfound04 <zeusnotfound0@gmail.com>
2025-05-29 12:02:33 +0530

    feat: enchanced the file enum technique

--- internal/executor/file_enum.go
+++ internal/executor/file_enum.go
@@ -2,20 +2,33 @@ package executor
 
 import (
 	"fmt"
-	"io/ioutil"
+	"os"
+	"strings"
 
 	"github.com/zeusnotfound04/DecoyOps/internal/logger"
 )
 
 func FileEnumeration() {
-	files, err := ioutil.ReadDir(".")
-
+	files, err := os.ReadDir(".")
 
 	if err != nil {
-		logger.Log("T1083" , "File and Directory Discovery" , "Failed to enumerate files")
+		logger.Log("T1083", "File and Directory Discovery", "Failed to enumerate files")
 		return
 	}
 
-	logger.Log("T1083" , "File and Directory Discovery" , fmt.Sprintf("Enumerated %d files in current directory", len(files)))
-}
+	var fileDetails []string
+	for _, file := range files {
+		info, err := file.Info()
+		if err == nil {
+			fileDetails = append(fileDetails, fmt.Sprintf("%s (Size: %d bytes, Modified: %s)",
+				file.Name(),
+				info.Size(),
+				info.ModTime().Format("2006-01-02 15:04:05")))
+		}
+	}
 
+	logger.Log("T1083", "File and Directory Discovery",
+		fmt.Sprintf("Enumerated %d files in current directory:\n- %s",
+			len(files),
+			strings.Join(fileDetails, "\n- ")))
+}
